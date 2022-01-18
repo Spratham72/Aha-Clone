a7940c9c3807a4780ba2f97e1511481f37f1a9ed
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-01-19 00:52:06 +0530

    aha Originals

---
+++ aha/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Aha_Original } from './components/aha_Originals';
+
+function App() {
+  return (
+    <div>
+      <Aha_Original />
+    </div>
+  );
+}
+
+export default App;
---
+++ aha/src/components/aha_Originals.jsx
@@ -0,0 +1,41 @@
+import { Div, Mdiv } from "../style/Div"
+
+export const Aha_Original = () => {
+
+
+    return (
+        <div>
+            <h1>aha Originals</h1>
+            <Div>
+                {/* 1 */}
+                <Mdiv>
+                    <img src="https://image-resizer-cloud-api.akamaized.net/image/AE9D806A-5950-4F13-8FAD-1CFF2AD7DABF/0-2x3.jpg?width=272" alt="The American Dream" />
+                </Mdiv>
+                {/* 2 */}
+                <Mdiv>
+                    <img src="https://image-resizer-cloud-api.akamaized.net/image/4BAD80A8-8D00-4010-B9E3-02F257B86B74/0-2x3.jpg?width=272" alt="Senapathi" />
+                </Mdiv>
+                {/* 3 */}
+                <Mdiv>
+                    <img src="https://image-resizer-cloud-api.akamaized.net/image/E51CB6A8-D3A9-474C-84E7-BDB6B2980239/0-2x3.jpg?width=272" alt="Unstoppable" />
+                </Mdiv>
+                {/* 4 */}
+                <Mdiv>
+                    <img src="https://image-resizer-cloud-api.akamaized.net/image/A7C4A8B8-B112-4C74-8AFC-6128093F71FC/0-2x3.jpg?width=272" alt="Chef mantra" />
+                </Mdiv>
+                {/* 5 */}
+                <Mdiv>
+                    <img src="https://image-resizer-cloud-api.akamaized.net/image/1D67B609-AC68-4A94-B80E-D0B8437B5519/0-2x3.jpg?width=272" alt="3 Roses" />
+                </Mdiv>
+                {/* 6 */}
+                <Mdiv>
+                    <img src="https://image-resizer-cloud-api.akamaized.net/image/836A0A87-656C-46FE-BBD1-F12BA6088F68/0-2x3.jpg?width=272" alt="SARKAAR" />
+                </Mdiv>
+                {/* 7 */}
+                <Mdiv>
+                    <img src="https://image-resizer-cloud-api.akamaized.net/image/0AA73A1E-4E26-45C5-9891-FBA787699AD4/0-2x3.jpg?width=272" alt="Alludu Gaaru" />
+                </Mdiv>
+            </Div>
+        </div>
+    );
+};
