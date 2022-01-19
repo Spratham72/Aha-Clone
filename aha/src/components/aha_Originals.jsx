e47b4889bf3e71d7364bc2a202aa3b438b75f812
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-01-19 09:43:39 +0530

    Update

--- aha/src/components/aha_Originals.jsx
+++ aha/src/components/aha_Originals.jsx
@@ -35,6 +35,34 @@ export const Aha_Original = () => {
                 <Mdiv>
                     <img style={{borderRadius:"12px"}} src="https://image-resizer-cloud-api.akamaized.net/image/0AA73A1E-4E26-45C5-9891-FBA787699AD4/0-2x3.jpg?width=272" alt="Alludu Gaaru" />
                 </Mdiv>
+                {/* 8 */}
+                <Mdiv>
+                    <img style={{borderRadius:"12px"}} src="https://image-resizer-cloud-api.akamaized.net/image/7C1146D3-79CA-47FF-AB88-B9721D445531/0-2x3.jpg?width=260" alt="The Baker & The Beauty" />
+                </Mdiv>
+                {/* 9 */}
+                <Mdiv>
+                    <img style={{borderRadius:"12px"}} src="https://image-resizer-cloud-api.akamaized.net/image/09E87D4B-120A-4971-8016-E177B373E66E/0-2x3.jpg?width=260" alt="Tharagathi Gadhi Daati" />
+                </Mdiv>
+                {/* 10 */}
+                <Mdiv>
+                    <img style={{borderRadius:"12px"}} src="https://image-resizer-cloud-api.akamaized.net/image/AE0B5149-A41D-492E-9031-9D00DA960641/0-2x3.jpg?width=260" alt="Maha Ganesha" />
+                </Mdiv>
+                {/* 11 */}
+                <Mdiv>
+                    <img style={{borderRadius:"12px"}} src="https://image-resizer-cloud-api.akamaized.net/image/85D56847-BEB7-4C31-8F03-72FB55EE3FA6/0-2x3.jpg?width=260" alt="Kudi Yedamaithe" />
+                </Mdiv>
+                {/* 12 */}
+                <Mdiv>
+                    <img style={{borderRadius:"12px"}} src="https://image-resizer-cloud-api.akamaized.net/image/CF86398B-F897-4DEA-B067-2AEC473A7309/0-2x3.jpg?width=260" alt="11th Hour" />
+                </Mdiv>
+                {/* 13 */}
+                <Mdiv>
+                    <img style={{borderRadius:"12px"}} src="https://image-resizer-cloud-api.akamaized.net/image/73842883-4C30-43B4-AC0D-232A4496E185/0-2x3.jpg?width=260" alt="In The Name of GOD" />
+                </Mdiv>
+                {/* 14 */}
+                <Mdiv>
+                    <img style={{borderRadius:"12px"}} src="https://image-resizer-cloud-api.akamaized.net/image/AB4E98DA-CAA5-48C0-81A8-DB20084EE026/0-2x3.jpg?width=260" alt="Locked" />
+                </Mdiv>
             </Div>
         </div>
     );
