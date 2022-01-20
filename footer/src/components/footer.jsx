6988a814877846af3f6f2fcf41203dc27c8e43c3
Ravi-Krt-Patel <ravikumar030494@gmail.com>
2022-01-20 15:39:33 +0530

    footer

--- footer/src/components/footer.jsx
+++ footer/src/components/footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
 
     return (<>
         <div className="o_container">
-            <div style={{ height: '5rem' }}></div>
+
             <div className="f_container f_flex">
                 <div className="f_left_sec">
                     <div>
@@ -179,19 +179,19 @@ export const Footer = () => {
                     </div>
                     <div className="f_s_must_watch">
                         <div>
-                            <img src="https://www.aha.video/assets/icons/svg/aha-footer-logo.svg" className="f_image" />
+                            <img src="https://www.aha.video/assets/icons/svg/aha-footer-logo.svg" alt="logo" className="f_image" />
                         </div>
                         <div>
                             <div className="text_font f_contect_us" >Connect with us</div>
                             <div>
-                                <a href="https://www.instagram.com/ahavideoin/" target="_blank"><img src="https://www.aha.video/assets/icons/svg/instagram-icon.svg" className="insta_logo" /></a>
+                                <a href="https://www.instagram.com/ahavideoin/" target="_blank"><img src="https://www.aha.video/assets/icons/svg/instagram-icon.svg" alt="logo" className="insta_logo" /></a>
                                 <a href="https://www.facebook.com/ahavideoIN" target="_blank">
-                                    <img src="https://www.aha.video/assets/icons/svg/facebook-footer.svg" className="insta_logo" />
+                                    <img src="https://www.aha.video/assets/icons/svg/facebook-footer.svg" alt="logo" className="insta_logo" />
                                 </a>
 
 
                                 <a href="https://twitter.com/AhavideoIN" target="_blank">
-                                    <img src="https://www.aha.video/assets/icons/svg/twitter-icon.svg" className="insta_logo" />
+                                    <img src="https://www.aha.video/assets/icons/svg/twitter-icon.svg" alt="logo" className="insta_logo" />
                                 </a>
 
                             </div>
@@ -200,11 +200,11 @@ export const Footer = () => {
                             <div className="text_font">Download aha mobile app</div>
                             <div>
                                 <a href="https://play.google.com/store/apps/details?id=ahaflix.tv">
-                                    <img src="https://www.aha.video/assets/images/googlePlay.png" className="f_g_play" />
+                                    <img src="https://www.aha.video/assets/images/googlePlay.png" className="f_g_play" alt="logo" />
                                 </a>
 
                                 <a href="https://apps.apple.com/in/app/aha-ott-movies-webseries/id1488739001">
-                                    <img src="https://www.aha.video/assets/images/ios.png" className="f_g_play" />
+                                    <img src="https://www.aha.video/assets/images/ios.png" className="f_g_play" alt="logo" />
                                 </a>
 
                             </div>
@@ -222,7 +222,7 @@ export const Footer = () => {
                 <div>
                     <div className="f_flex powered_sec">
                         <h5 style={{ color: "#96969d", marginTop: "0px", marginRight: "2vw", fontSize: "1.2rem" }}>Powered by</h5>
-                        <img className="powered_logo2" src="https://www.aha.video/assets/images/fl_logo.png" />
+                        <img className="powered_logo2" src="https://www.aha.video/assets/images/fl_logo.png" alt="logo" />
                     </div>
                     <div style={{ textAlign: 'center', fontSize: "1.4rem", color: 'white' }} > © Copyright 2022 Arha Media and Broadcasting Pvt Ltd. All rights reserved. </div>
                 </div>
@@ -233,15 +233,49 @@ export const Footer = () => {
 
         <div className="f_nav_container">
             <div className="f_nav_top">
-                <div>
-                    thicdfdfdsfd
-                    <div>ddsafdsadsafds</div>
+                <div className="f_nav_top_logo">
+                    <img src="https://www.aha.video/aha-logo.db810aeaa42b356a86a7.png" alt="logo" width="100%" />
                 </div>
-            </div>
-            <div className="f_nav_bottom">
+                <div className="f_nav_top_logo2">
+                    <img src="https://www.aha.video/language-icon.ef88ebcc6b1bcda97fc4.svg" alt="logo" width="100%" />
+                </div>
+                <div style={{
+                    display: "flex",
+                    justifyContent: "space-around"
+                }} >
+                    <div className="f_nav_top_child">
+                        All
+                    </div>
+                    <div className="f_nav_top_child">
+                        Movies
+                    </div>
+                    <div className="f_nav_top_child">
+                        Shows
+                    </div>
+                    <div className="f_nav_top_child">
+                        Kids
+                    </div>
+                </div>
 
             </div>
-            <div style={{ height: '5rem' }}></div>
+            <div className="f_nav_bottom">
+                <a href="#">
+                    <div className="f_nav_bottom_child">Home</div>
+                </a>
+                <a href="#">
+                    <div className="f_nav_bottom_child">My aha</div>
+                </a>
+
+
+                <a href="#">
+                    <div className="f_nav_bottom_child">Seach</div>
+                </a>
+
+                <a href="#">
+                    <div className="f_nav_bottom_child">Menu</div>
+                </a>
+
+            </div>
         </div>
 
 
